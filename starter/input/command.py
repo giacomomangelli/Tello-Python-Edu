eaa4e27326225ec1be93e0e47f3083a2f90cc74c
Giacomo-it <giacomomsidp@gmail.com>
2025-01-19 17:39:42 +0100

    wip: init tello integration

---
+++ starter/input/command.py
@@ -0,0 +1,5 @@
+from starter.tello.tello import Tello
+
+
+def start(command):
+    tello = Tello()
--- starter/main.py
+++ starter/main.py
@@ -1,5 +1,4 @@
-def on_key_press(test):
-    print(f"You pressed ", test)
+
 
 def main():
     on_key_press(input("prova: "))
